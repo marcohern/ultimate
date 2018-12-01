21de4046a8348d939041bf8950d98353ac6a0213
Marco Hernandez <marcohern@gmail.com>
2018-12-01 03:13:00 -0500

    added categories

--- backend/database/migrations/2018_12_01_074457_create_categories_table.php
+++ backend/database/migrations/2018_12_01_074457_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,23 +13,22 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',128);
             $table->string('slug',128)->unique();
-            $table->boolean('active')->default(true);
-            $table->longText('description');
-            $table->decimal('org_price',18,2);
-            $table->decimal('dct_price',18,2)->nullable();
-            $table->integer('qty')->default(0);
-            $table->integer('prev_id')->nullable();
-            
+            $table->integer('parent_id')->nullable();
+            $table->string('type')->default('category');
+            $table->integer('ord')->default(0);
+            $table->integer('lvl')->default(0);
+            $table->string('description');
+
             $table->integer('hits')->default(0);
             $table->integer('clicks')->default(0);
             $table->integer('sales_count')->default(0);
             $table->decimal('sales_value',18,2)->default(0);
-            $table->decimal('stars',2,1)->default(0.0);
-            $table->integer('comments')->default(0);
+
+            $table->index('parent_id');
 
             $table->timestamps();
         });
@@ -42,6 +41,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 }
