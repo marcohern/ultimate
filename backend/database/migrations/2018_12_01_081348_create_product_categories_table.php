cb62443d097cae8dd2c6bd69f82be64b6c881e1c
Marco Hernandez <marcohern@gmail.com>
2018-12-01 03:46:30 -0500

    categories and product categories tables created

---
+++ backend/database/migrations/2018_12_01_081348_create_product_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id',false,true)->length(10)->default(0);
+            $table->integer('category_id',false,true)->length(10)->default(0);
+
+            $table->unique(['product_id','category_id']);
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
