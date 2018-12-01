d5e533e9b1f2e0e05cc845defa3eee06cc5103fe
Marco Hernandez <marcohern@gmail.com>
2018-12-01 02:27:38 -0500

    renamed table product to products

--- backend/database/migrations/2018_11_30_025249_create_products_table.php
+++ backend/database/migrations/2018_11_30_025249_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',128);
             $table->string('slug',128)->unique();
