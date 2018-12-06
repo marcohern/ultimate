a13c80898914160d3583ed94e53654fadf3f60e6
Marco Hernandez <marcohern@gmail.com>
2018-12-06 11:41:43 -0500

    fixed category slider

--- ult-brezza/src/app/cmps/catslider/catslider.component.ts
+++ ult-brezza/src/app/cmps/catslider/catslider.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery;
+
 @Component({
   selector: 'brezza-catslider',
   templateUrl: './catslider.component.html',
@@ -10,6 +12,19 @@ export class CatsliderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    /* Category desc slider */
+    jQuery("#category-desc-slider .slider-items").owlCarousel({
+      autoPlay: true,
+      items: 1, //10 items above 1000px browser width
+      itemsDesktop: [1024, 1], //5 items between 1024px and 901px
+      itemsDesktopSmall: [900, 1], // 3 items betweem 900px and 601px
+      itemsTablet: [600, 1], //2 items between 600 and 0;
+      itemsMobile: [320, 1],
+      navigation: true,
+      navigationText: ["<a class=\"flex-prev\"></a>", "<a class=\"flex-next\"></a>"],
+      slideSpeed: 500,
+      pagination: false
+    });
   }
 
 }
--- ult-brezza/src/assets/js/common.js
+++ ult-brezza/src/assets/js/common.js
@@ -75,19 +75,6 @@ jQuery(document).ready(function() {
 		slideSpeed: 500,
 		pagination: false
 	});
-	/* Category desc slider */
-	jQuery("#category-desc-slider .slider-items").owlCarousel({
-		autoPlay: true,
-		items: 1, //10 items above 1000px browser width
-		itemsDesktop: [1024, 1], //5 items between 1024px and 901px
-		itemsDesktopSmall: [900, 1], // 3 items betweem 900px and 601px
-		itemsTablet: [600, 1], //2 items between 600 and 0;
-		itemsMobile: [320, 1],
-		navigation: true,
-		navigationText: ["<a class=\"flex-prev\"></a>", "<a class=\"flex-next\"></a>"],
-		slideSpeed: 500,
-		pagination: false
-	});
 	/* Related products slider */
 	jQuery("#related-products-slider .slider-items").owlCarousel({
 		items: 4, //10 items above 1000px browser width
