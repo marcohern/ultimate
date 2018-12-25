43a2c6579c328567a7197fb7392d3d015991b157
Marco Hernandez <marcohern@gmail.com>
2018-12-25 15:50:29 -0500

    fixint service tests in core

--- frontend/projects/marcohern/ultimate-core/src/lib/srvs/request.service.spec.ts
+++ frontend/projects/marcohern/ultimate-core/src/lib/srvs/request.service.spec.ts
@@ -1,9 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 import { RequestService } from './request.service';
 
 describe('RequestService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let requestStup:any = {
+
+  };
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports:[HttpClientTestingModule],
+    providers:[]
+  }));
 
   it('should be created', () => {
     const service: RequestService = TestBed.get(RequestService);
