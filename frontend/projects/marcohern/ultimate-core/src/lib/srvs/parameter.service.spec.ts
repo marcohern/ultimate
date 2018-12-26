dfb0a54c1724c36298efd0f5279cf41675002439
Marco Hernandez <marcohern@gmail.com>
2018-12-26 01:08:51 -0500

    adding unit tests on services

--- frontend/projects/marcohern/ultimate-core/src/lib/srvs/parameter.service.spec.ts
+++ frontend/projects/marcohern/ultimate-core/src/lib/srvs/parameter.service.spec.ts
@@ -1,16 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ParameterService } from './parameter.service';
+import { of } from 'rxjs/observable/of';
+import { Parameter, RequestService } from '@marcohern/ultimate-core';
+
 
 describe('ParameterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports:[HttpClientTestingModule]
-  }));
+  let param1:Parameter = {
+    id:1 ,
+    name: 'param1',
+    group: 'group',
+    value: 'value'
+  };
+  let service: ParameterService;
+  let httpMock: HttpTestingController;
+  let pss:any;
+  
+  beforeEach(() => {
+    pss = {
+      get: () => of([param1]),
+    };
+
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers: [
+        ParameterService,
+        {provide: RequestService, useValue: pss}
+      ]
+    });
+
+    service = TestBed.get(ParameterService);
+    httpMock = TestBed.get(httpMock);
+  });
 
   it('should be created', () => {
-    const service: ParameterService = TestBed.get(ParameterService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get parameter', () => {
+    service.getAllParameters().subscribe(result => {
+      expect(result.length).toBe(1);
+    });
+
+    const request = httpMock.expectOne('/parameters');
   });
 });
